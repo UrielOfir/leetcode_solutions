2aa24f54fa5b4c8c57af205c631278199e2c25e2
Uriel Ofir <urielofir@gmail.com>
2023-11-06 07:59:35 +0200

    Seat Reservation Manager

---
+++ seat-reservation-manager.ts
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/seat-reservation-manager/
+
+class SeatManager {
+    private seats: number[];
+    private availableSeats: Set<number>;
+
+    constructor(n: number) {
+        this.seats = Array.from({length: n}, (_, i) => i + 1);
+        this.availableSeats = new Set(this.seats);
+    }
+
+    reserve(): number {
+        const seat = Math.min(...this.availableSeats);
+        this.availableSeats.delete(seat);
+        return seat;
+    }
+
+    unreserve(seatNumber: number): void {
+        this.availableSeats.add(seatNumber);
+    }
+}
